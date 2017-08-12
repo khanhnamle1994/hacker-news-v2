b01c60b47d61a8ae6c863e628ee4834dead430f6
James Le <le_j6@denison.edu>
2017-08-12 15:20:03 -0700

    Interactions with Forms and Events

--- src/App.js
+++ src/App.js
@@ -20,15 +20,28 @@ const list = [
   },
 ];
 
+function isSearched(searchTerm) {
+  return function(item) {
+    return !searchTerm ||
+      item.title.toLowerCase().includes(searchTerm.toLowerCase());
+  }
+}
+
 class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
       list,
+      searchTerm: '',
     };
 
+    this.onSearchChange = this.onSearchChange.bind(this);
     this.onDismiss = this.onDismiss.bind(this);
+  }
+
+  onSearchChange(event) {
+    this.setState({ searchTerm: event.target.value });
   }
 
   onDismiss(id) {
@@ -40,7 +53,13 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        { this.state.list.map(item =>
+        <form>
+          <input
+            type="text"
+            onChange={this.onSearchChange}
+          />
+        </form>
+        { this.state.list.filter(isSearched(this.state.searchTerm)).map(item =>
             <div key={item.objectID}>
               <span>
                 <a href={item.url}>{item.title}</a>
