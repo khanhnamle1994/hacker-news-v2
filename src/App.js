a1105d0dffd6c22b5e8e28ab37bb448aaf25485f
James Le <le_j6@denison.edu>
2017-08-12 15:36:26 -0700

    Component Declarations

--- src/App.js
+++ src/App.js
@@ -70,20 +70,14 @@ class App extends Component {
   }
 }
 
-class Search extends Component {
-  render() {
-    const { value, onChange, children } = this.props;
-    return (
-      <form>
-        {children} <input
-          type="text"
-          value={value}
-          onChange={onChange}
-        />
-      </form>
-    );
-  }
-}
+const Search = ({ value, onChange, children }) =>
+  <form>
+    {children} <input
+      type="text"
+      value={value}
+      onChange={onChange}
+    />
+  </form>
 
 class Table extends Component {
   render() {
