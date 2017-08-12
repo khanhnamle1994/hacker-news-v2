7b04d24ca6fb5edbb9fa9eff4ec2f620abb748ff
James Le <le_j6@denison.edu>
2017-08-12 15:23:17 -0700

    Controlled Components

--- src/App.js
+++ src/App.js
@@ -57,6 +57,7 @@ class App extends Component {
         <form>
           <input
             type="text"
+            value={searchTerm}
             onChange={this.onSearchChange}
           />
         </form>
